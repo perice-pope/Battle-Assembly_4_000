e7ef3c0b52bbed437d839703bf8cfec58088e865
Perice Pope <pericepope@Catherines-MacBook-Air.local>
2019-08-06 17:23:39 -0700

    added event clicks

--- js.js/main.js
+++ js.js/main.js
@@ -1,5 +1,3 @@
-// alert('job'); 
-
 
 /*----- constants -----*/ 
 // const missAudio = newAudio('onlinemp3'); 
@@ -8,7 +6,7 @@
 // const winnerAudio = newAudio('onlinemp3');
 
 /*----- app's state (variables) -----*/ 
-let score, miss, hit, sink, winner; 
+let score, miss, hit, sink, winner, currentShipId; 
 
 var board1 = [
     [0,0,0,0,0,0,0,0,0,0],
@@ -93,18 +91,24 @@ var players = {
 
 /*----- cached element references -----*/ 
 var vCrusierPic = document.getElementById('ship1v'); 
+var vBattleshipPic = document.getElementById('ship2v'); 
+var vDestroyerPic = document.getElementById('ship3v'); 
+var vRescuePic = document.getElementById('ship4v'); 
 
 /*----- event listeners -----*/
-document.querySelectorAll('td')
-.forEach(e => e.addEventListener("click", clickHandler));
+document.querySelectorAll('td').forEach(e => e.addEventListener("click", clickHandler));
 
 // image clicks
 vCrusierPic.addEventListener("click", imgClicks);
+vBattleshipPic.addEventListener("click", imgClicks);
+vDestroyerPic.addEventListener("click", imgClicks);
+vRescuePic.addEventListener("click", imgClicks);
 
 // give each ship their length attributes on click. 
-var shipObjLength = players.cpu.ships.crusier.length;
- 
-
+var cruObjLength = players.cpu.ships.crusier.length;
+var batObjLength = players.cpu.ships.battleship.length;
+var desObjLength = players.cpu.ships.destroyer.length;
+var resObjLength = players.cpu.ships.rescue.length;
 
 
 /*----- functions -----*/
@@ -118,19 +122,40 @@ function render() {
 
 } 
 
-function imgClicks() {
-// assign length to image. to move to board... 
-console.log(shipObjLength);   
-
+function imgClicks(e) {   
+// get id of image. 
+console.log(e.target.id); 
+currentShipId = e.target.id; 
 }
 
 // here me out OR...
-function clickHandler() {
-    // should be on each td box broooo... 
-    console.log("clicked"); 
+function clickHandler(e) {
+    console.log("you clicked");
+    console.log(e.target.id); 
+    placeShips(currentShipId); 
+    document.getElementById(e.target.id).style.backgroundColor = "red";
+
+    var ele = e.target.id; 
+    console.log(Element.closest(ele)); 
+   
+    //now find the index to the left & affect it according to the currentShipId. 
+    // if (currentShipId === 2) {
+    // ele = ele.closest(ele);
+    // } else if (currentShip === 3) {
+    // closet 3 elements }
+    // bla bla bla 
+    // else if (currentShip === 4)
+    // closet 4 elements
+    // else (currentShip === 5)
+    // closet 5 elements  
 }
 
-function placeShips(ships, x, y) {
+function placeShips(n) {
+// allow me to place ship. takes in ID number.
+console.log(n);   
+// on click of spot on board, place n amount of markers starting at that idx.
+// so get that idx or spot on the board.   
+// board[0][0] while n = 2 place markers on board 00 and b01 
 
 }
 
